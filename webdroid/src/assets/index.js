bf9db288bd2fabd4bdd04b72eab588b99492a22a
Khaerul Anam <khaerulanam4923@gmail.com>
2023-11-17 15:20:04 +0700

    Fix Navbar

--- webdroid/src/assets/index.js
+++ webdroid/src/assets/index.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import menu from "./menu.png";
 import medis from "./medis.png";
 import close from "./close.svg";
+import notif from "./notif.svg";
+import profile from "./profile.svg";
 import medication from "./Medication.svg";
 
 export {
@@ -10,4 +12,6 @@ export {
     close,
     menu,
     medis,
+    notif,
+    profile,
 }
